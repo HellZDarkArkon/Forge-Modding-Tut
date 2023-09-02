b2c3adf8196c0e082a34c17f65c140491ce56103
HellZDarkArkon <maximumwerewolf@gmail.com>
2023-09-02 17:52:21 -0400

    set up everything up to loot tables.

--- src/main/java/com/github/hellzdarkarkon/src/tutorialmod/TutorialMod.java
+++ src/main/java/com/github/hellzdarkarkon/src/tutorialmod/TutorialMod.java
@@ -1,6 +1,10 @@
-package com.github.hellzdarkarkon.src.forgemodtut;
+package com.github.hellzdarkarkon.src.tutorialmod;
 
+import com.github.hellzdarkarkon.src.tutorialmod.block.ModBlocks;
+import com.github.hellzdarkarkon.src.tutorialmod.item.ModCreativeModeTabs;
+import com.github.hellzdarkarkon.src.tutorialmod.item.ModItems;
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -13,14 +17,18 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-@Mod(ForgeModTut.MODID)
-public class ForgeModTut {
+@Mod(TutorialMod.MODID)
+public class TutorialMod {
 
     public static final String MODID = "tutorialmod";
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    public ForgeModTut() {
+    public TutorialMod() {
         IEventBus modEB = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModCreativeModeTabs.register(modEB);
+        ModItems.register(modEB);
+        ModBlocks.register(modEB);
 
         modEB.addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
@@ -34,7 +42,10 @@ public class ForgeModTut {
     }
 
     private void addCreative(BuildCreativeModeTabContentsEvent ev) {
-
+        if(ev.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            ev.accept(ModItems.SAPPHIRE);
+            ev.accept(ModItems.RAW_SAPPHIRE);
+        }
     }
 
     @SubscribeEvent
